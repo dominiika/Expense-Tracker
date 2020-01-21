ffbed7f2b394b1db2a6011961be5d78f49fa7b77
Dosia <dominika.gajda@gmail.com>
2020-01-21 12:31:24 +0100

    Initial Heroku commit

---
+++ expensetracker/settings/production.py
@@ -0,0 +1,18 @@
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+DEBUG = False
+ALLOWED_HOSTS = ['expensetracker-django.herokuapp.com']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
